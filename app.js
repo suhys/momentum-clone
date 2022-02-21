6a51dc58a179c2d3a91eade4ccf20b203ac3916b
suhys <yuseung.suh@gmail.com>
2022-02-21 14:41:06 -0800

    Update Login - Saving Username

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
-const loginForm = document.querySelector(".login-form");
-const loginInput = loginForm.querySelector("input");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
+  const typedUserName = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, typedUserName);
+  paintGreetings(typedUserName);
+}
+
+function paintGreetings(username) {
   greeting.innerText = "Hello " + username;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreetings(savedUsername);
+}

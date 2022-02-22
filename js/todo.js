3fd0b301528f0b2735e3ead83e68bba8e9498369
suhys <yuseung.suh@gmail.com>
2022-02-22 10:43:31 -0800

    Update todo.js - Adding Todos

--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,20 @@
-const toDoForm = document.getElementByID("todo-form");
-const toDoInput = toDoForm.querySelector("input");
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+}
 
 function handleToDoSubmit(event) {
   event.preventDefault();
-  const newTodo = "toDoInput.value";
+  const newTodo = toDoInput.value;
   toDoInput.value = "";
-  console.log(newToDo, toDoInput.value);
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);

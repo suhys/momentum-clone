16b44b6051b613d0b239750ea2f1d89137dcef7a
suhys <yuseung.suh@gmail.com>
2022-03-04 12:46:47 -0800

    Create weather.js

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const APIKEY = "bc4f662bfd5e2b961e33acd63594ac58";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${APIKEY}&units=imperial`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
